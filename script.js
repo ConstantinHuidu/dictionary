1c553c6ed57bab265af5505cb862b667b7c882a0
Constantin Huidu <constantin.huidu@gmail.com>
2022-01-11 11:09:48 +0200

    uploading first version

---
+++ script.js
@@ -0,0 +1,21 @@
+let wordsArray = [];
+let translatedWordsArray = [];
+
+function addWords() {
+	wordsArray.push(document.getElementById('word').value);
+	translatedWordsArray.push(document.getElementById('translation').value);
+	document.getElementById('word').value = "";
+	document.getElementById('translation').value = "";
+}
+
+function searchWords() {
+	var searchBy = document.getElementById('searchWord').value;
+	var posWord = wordsArray.indexOf(searchBy);
+	var translation = translatedWordsArray[posWord];
+	if (wordsArray.includes(searchBy)) {
+		document.getElementById('searchResult').innerHTML = `You are searching for: <strong>` + searchBy + `</strong>. Translation for this word is: <strong>` + translation + `</strong>`;
+	} else {
+		document.getElementById('searchResult').innerHTML = `The word <strong>` + searchBy + `</strong> couldn't be found in the dictionary`;
+	}
+	document.getElementById('searchWord').value = "";
+}
